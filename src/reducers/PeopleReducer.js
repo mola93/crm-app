3e287869947e801b7a8c22261f1cf35181321662
Molamin <Molamin73@gmail.com>
2019-01-31 21:14:35 +0000

    People Reducer

--- src/reducers/PeopleReducer.js
+++ src/reducers/PeopleReducer.js
@@ -2,10 +2,27 @@ import people from './people.json';
 
 const initialState  = {
     people,
+    detailView: false,
+    personSelected: null,
 };
 
 export default ( state = initialState, action) => {
     switch(action.type){
+
+        case 'SELECTED_PERSON':
+          return {
+            ...state,
+            detailView: true,
+            personSelected: action.payload
+          }
+
+
+          case 'NONE_SELECTED':
+          return {
+            ...state,
+            detailView: false,
+            personSelected: null,
+          }
         default:
         return state;
     }
