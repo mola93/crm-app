56a91de59470e97c192d37e716e56e617c1b002a
Molamin <Molamin73@gmail.com>
2019-01-31 22:40:16 +0000

    per person detail view with css

---
+++ src/PeopleDetail.js
@@ -0,0 +1,180 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {   Text, View, StyleSheet, Image, ScrollView, TouchableOpacity, Linking} from 'react-native';
+import {connect } from 'react-redux';  
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import SimpleIcon from 'react-native-vector-icons/SimpleLineIcons';
+import * as actions from './actions';
+import {getTheme} from 'react-native-material-kit';
+
+const theme = getTheme();
+
+
+const styles = StyleSheet.create({
+    card: {
+      marginTop: 10,
+      paddingBottom: 20,
+      marginBottom: 20,
+      borderColer: 'lightgrey',
+      borderWidth: 0.5,
+    },
+    title1: {
+        top: 10,
+        left: 80,
+        fontSize: 24,
+    },
+    title2: {
+        top: 35,
+        left: 82,
+        fontSize: 18,
+    },
+    image: {
+        flex: 0,
+        height: 100,
+        width: 333,
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItem: 'center',
+    },
+    closeIcon: {
+        position: 'absolute',
+        top: 5,
+        left: 295,
+        color: 'rgba(233,166,154,0.8)',
+        backgroundColor: 'rgba(255,255,255,0)',
+    },  
+    icon: {
+        position: 'absolute',
+        top: 15,
+        left: 0,
+        color: 'white',
+        backgroundColor: 'rgba(255,255,255,0)',
+    },
+    textArea: {
+        flexDirection: 'row',
+        paddingLeft: 20,
+        paddingTop: 10,
+        width: 260,
+    },
+    textIcons: {
+        color: '#26a69a',
+    },
+    actionArea: {
+        paddingTop: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+  });
+  
+ class PeopleDetail extends Component {
+  handleClick = (link) => {
+      Linking.canOpenURL(link).then( supported => {
+          if (supported) {
+              Linking.openURL(link)
+          } else {
+              console.log('Don\'t know how to open URI: ' + link);
+          }
+      })
+  }
+
+
+
+  render() {
+    return (
+       <ScrollView showsVerticalScrollIndicator= {false}>
+
+       <Image
+             source={require('./images/background.jpg')}
+             style={[theme.cardImageStyle, styles.image]}
+       />
+       <EvilIcon name={'user'} size={100} style={styles.icon} />
+       <SimpleIcon name={'close'} size={30} style={styles.closeIcon}
+            onPress={() => {
+                this.props.nonSelected()
+            }}
+       />
+       <Text style={[theme.cardTitleStyle, styles.title1]}> {this.props.person.first_name} {this.props.person.last_name} </Text>
+       <Text style={[theme.cardTitleStyle, styles.title2]}> from {this.props.person.company} </Text>
+        <View>
+            <MaterialIcon name={'phone'} size={40} style={styles.textIcons} />
+            <Text style={[theme.cardContentStyle]}> {this.props.person.phone} </Text>
+
+        </View>
+
+        <View>
+            <MaterialIcon name={'email'} size={40} style={styles.textIcons} />
+            <Text style={[theme.cardContentStyle]}> {this.props.person.email} </Text>
+
+        </View>
+
+        <View>
+            <MaterialIcon name={'assignment'} size={40} style={styles.textIcons} />
+            <Text style={[theme.cardContentStyle]}> {this.props.person.project} </Text>
+
+        </View>
+        <View>
+            <MaterialIcon name={'mode-edit'} size={40} style={styles.textIcons} />
+            <Text style={[theme.cardContentStyle]}> {this.props.person.notes} </Text>
+
+        </View>
+<View style={styles.actionArea}>
+ <TouchableOpacity 
+          onPress={() => {
+
+         this.handleClick('tel:${this.props.person.phone}')
+          }}
+         >
+        <Image source={require('./images/call@2x.png')} style={styles.actionImage} />
+</TouchableOpacity>
+
+<TouchableOpacity 
+          onPress={() => {
+
+         this.handleClick('mail:${this.props.person.email}')
+          }}
+         >
+        <Image source={require('./images/email@2x.png')} style={styles.actionImage} />
+</TouchableOpacity>
+
+
+<TouchableOpacity 
+          onPress={() => {
+
+         this.handleClick('tel:${this.props.person.phone}')
+          }}
+         >
+        <Image source={require('./images/call@2x.png')} style={styles.actionImage} />
+</TouchableOpacity>
+<View style={styles.actionArea}>
+              <Text>Call</Text>
+              <Text>SMS</Text>
+              <Text>Email</Text>
+</View>
+
+</View>
+
+       </ScrollView>
+    );
+  }
+}
+
+
+// this lifecycle hook makes the data available to the component
+const mapStateToProps = state => {
+
+  return { people: state.people,
+           personSelected: state.personSelected
+
+};
+}
+
+export default connect(mapStateToProps, actions)(PeopleDetail)
