abc9b0c0a5ceb5d7bf7f92b738c9d341a161cf7d
Molamin <Molamin73@gmail.com>
2019-01-31 10:59:47 +0000

    updated gradle

--- src/AddPerson.js
+++ src/AddPerson.js
@@ -1,61 +1,50 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- *
- * @format
  * @flow
  */
 
-import React, {Component} from 'react';
-import {   Text, View, StyleSheet} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/EvilIcons';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexWrap: 'wrap',
+    paddingTop: 20,
+    backgroundColor: '#e5e5e5',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    margin: 10,
+  },
+  icon: {
+      paddingBottom: 2,
+  },
+});
 
-const styles = StyleSheet.create(
-  {
-    container:{
-      flex:1,
-      width:353,
-      flexWrap:'wrap',
-      paddingTop:20,
-      backgroundColor: '#e5e5e5',
-    },
-    welcome:{
-        fontSize: 20,
-        textAlign: 'center',
-        textAlignVertical: 'center',
-        margin: 10,
-      },
-      icon: {
-          paddingBottom:2
-      },
-  }
-);
-
- export default class AddPerson extends Component {
-
+export default class AddPerson extends Component {
   static navigationOptions = {
-   
-        tabBarlabel: 'Add Person',
-        tabBaricon: ({tintColor}) =>  (
-            <Icon 
-            name={'plus'}
-            size={70}
-            style={[{ color:tintColor},styles.icon]} />
-          )
-           
-          
-      
-  }
+
+    tabBarLabel: 'Add person',
+    tabBarIcon: ({tintColor}) => (
+      <Icon 
+      name={'plus'}
+      size={50}
+      style={{ color:tintColor}} />
+    )
+
+    
+
+}
   render() {
     return (
-      <View style={styles.container} >
-         <Text>
-             Add person
-         </Text>
- 
-       </View>
+      <View style={styles.container}>
+        <Text>Add person</Text>
+      </View>
     );
   }
 }
-/* kggjgj */
--- src/CompanyList.js
+++ src/CompanyList.js
@@ -1,57 +1,47 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- *
- * @format
  * @flow
  */
 
-import React, {Component} from 'react';
-import {   Text, View, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexWrap: 'wrap',
+    paddingTop: 10,
+    backgroundColor: '#e5e5e5',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    margin: 10,
+  },
+});
 
-const styles = StyleSheet.create(
-  {
-    container:{
-      flex:1,
-      flexWrap:'wrap',
-      paddingTop:10,
-      backgroundColor: '#e5e5e5',
-    },
-    welcome:{
-        fontSize: 20,
-        textAlign: 'center',
-        textAlignVertical: 'center',
-        margin: 10,
-      },
-
-  }
-);
-
- export default class CompanyList extends Component {
-
+export default class AddPerson extends Component {
   static navigationOptions = {
-  
-        tabBarlabel: 'Companies',
-        tabBaricon: ({tintColor}) => (
-            <Icon 
-            name={'business'}
-            size={45}
-            style={{ color:tintColor}} />
-          )
-     
-          
-   
-  }
+
+    tabBarLabel: 'companies',
+    tabBarIcon: ({tintColor}) => (
+      <Icon 
+      name={'business'}
+      size={50}
+      style={{ color:tintColor}} />
+    )
+
+    
+
+}
   render() {
     return (
-      <View style={styles.container} >
-         <Text>
-           Companies
-         </Text>
- 
-       </View>
+      <View style={styles.container}>
+        <Text>Companies</Text>
+      </View>
     );
   }
 }
