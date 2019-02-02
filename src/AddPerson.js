cdfe3fa2c39b2bef11a9e02bebe1a848aacea6ee
Molamin <Molamin73@gmail.com>
2019-02-02 23:16:53 +0000

    Designed AddPerson form

--- src/AddPerson.js
+++ src/AddPerson.js
@@ -5,28 +5,33 @@
  */
 
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/EvilIcons';
+import {MKTextField, MKColor, MKButton } from  'react-native-material-kit'
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexWrap: 'wrap',
-    paddingTop: 20,
-    backgroundColor: '#e5e5e5',
+  form:{
+    flex:1,
+    paddingTop: 50,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight:20,
+    justifyContent: 'space-between',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    margin: 10,
-  },
-  icon: {
-      paddingBottom: 2,
-  },
+
+   fieldStyles: {
+      height: 40,
+      color: MKColor.Orange
+   },
+   addButton: {
+     marginTop: 20,
+   }
 });
 
-export default class AddPerson extends Component {
+const AddButton = MKButton.coloredButton().withText('ADD').build();
+
+class AddPerson extends Component {
   static navigationOptions = {  
 
     
@@ -44,9 +49,56 @@ export default class AddPerson extends Component {
 }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Add person</Text>
-      </View>
+     <ScrollView showsVerticalScrollIndicator={false}>
+       <View style={styles.form}>
+       <Text>Add a new contact</Text>
+       <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'first name...'} 
+          tintColor={MKColor.Teal}
+       />
+
+<MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'last name...'} 
+          tintColor={MKColor.Teal}
+       />
+
+<MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'Phone number...'} 
+          tintColor={MKColor.Teal}
+       />
+
+<MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'email ...'} 
+          tintColor={MKColor.Teal}
+       />
+
+<MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'company ...'} 
+          tintColor={MKColor.Teal}
+       />
+        <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'project ...'} 
+          tintColor={MKColor.Teal}
+       />
+
+<MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'notes ...'} 
+          tintColor={MKColor.Teal}
+       />
+       <View style={styles.addButton}>
+          <AddButton />
+       </View>
+       </View>
+     </ScrollView>
     );
   }
 }
+
+export default AddPerson;
