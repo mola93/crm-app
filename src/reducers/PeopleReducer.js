640d45e791ff5c04ce1d96cbf4e88c1f5b8aa296
Molamin <Molamin73@gmail.com>
2019-02-03 02:06:13 +0000

    Add person redux actions and reducers

--- src/reducers/PeopleReducer.js
+++ src/reducers/PeopleReducer.js
@@ -4,6 +4,15 @@ const initialState  = {
     people,
     detailView: false,
     personSelected: null,
+    first_name: '',
+    last_name: '',
+    phone: '',
+    email: '',
+    company: '',
+    project: '',
+    notes: '',
+    toUpdate: false,
+
 };
 
 export default ( state = initialState, action) => {
@@ -23,6 +32,20 @@ export default ( state = initialState, action) => {
             detailView: false,
             personSelected: null,
           }
+          case 'FORM_UPDATE':
+          return {
+              ...state,
+              [action.payload.prop]: action.payload.value
+          };
+           case 'NEW_CONTACT':
+               return initialState;
+
+           case 'ADD_PERSON':
+               return {
+                   ...state,
+                   ...action.newPerson
+               };
+      
         default:
         return state;
     }
